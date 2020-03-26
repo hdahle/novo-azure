0cbffe3fea223c9a25aba8cbfad9ef3a988ca4df
Håkon Dahle <hakon.dahle@gmail.com>
2020-03-26 09:33:48 +0100

    trying redis

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // create an express app
 const express = require("express")
 const app = express()
+var redis = require("redis");
 
 // use the express-static middleware
 app.use(express.static("public"))
@@ -18,6 +19,13 @@ app.get("/api", function (req, res) {
 // start the server listening for requests
 app.listen(process.env.PORT || 3000,
     () => {
+        let client = redis.createClient(6380,
+            process.env.REDISCACHEHOSTNAME,
+            {
+                auth_pass: process.env.REDISCACHEKEY,
+                tls: { servername: process.env.REDISCACHEHOSTNAME }
+            });
+        console.log("redis client: ", client)
         console.log("Server is listening on port 3000 (possibly)...");
     }
 );
